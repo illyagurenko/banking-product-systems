73a06f7e17b3af23639875763c4d655493815fdf
IlyaGurenko <illya4gurenko@gmail.com>
2026-06-02 15:58:00 +0300

    add struct scoring service

---
+++ src/main/java/ru/mybank/system/scoring/service/ScoringService.java
@@ -0,0 +1,5 @@
+package ru.mybank.system.scoring.service;
+
+@Service
+public class ScoringService {
+}
---
+++ src/main/java/ru/mybank/system/scoring/service/rule/JuniorAgeRule.java
@@ -0,0 +1,4 @@
+package ru.mybank.system.scoring.service.rule;
+
+public class JuniorAgeRule {
+}
